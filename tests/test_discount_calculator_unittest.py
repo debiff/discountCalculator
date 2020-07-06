3c936a069f720f01a48eefb36c0f18227de9bbd8
Simone Biffi <simone.biffi.91@gmail.com>
2020-07-06 17:04:30 +0200

    test: add test trivial test on void inputs

--- tests/test_discount_calculator_unittest.py
+++ tests/test_discount_calculator_unittest.py
@@ -1,5 +1,6 @@
 import unittest
 import json
+from discount_calculator import compute_discount
 
 
 class TestDiscountMethod(unittest.TestCase):
@@ -9,6 +10,23 @@ class TestDiscountMethod(unittest.TestCase):
 
         with open("./samples/products.json") as f:
             self.products_list = list(json.load(f))
+
+    def test_void_bundle(self):
+        cart = "ABC"
+        total_discount = compute_discount([], self.products_list, cart)
+        self.assertEqual(total_discount, 0)
+
+    def test_void_product_list(self):
+        cart = "ABC"
+        total_discount = compute_discount(self.discount_bundles, [], cart)
+        self.assertEqual(total_discount, 0)
+
+    def test_void_cart(self):
+        cart = ""
+        total_discount = compute_discount(
+            self.discount_bundles, self.products_list, cart
+        )
+        self.assertEqual(total_discount, 0)
 
 
 if __name__ == "__main__":
