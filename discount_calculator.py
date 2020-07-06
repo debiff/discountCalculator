e67b738e1f75c2322a94d23c8b529d1c9e46570a
Simone Biffi <simone.biffi.91@gmail.com>
2020-07-06 17:20:33 +0200

    doc: add compute_discount docstring

--- discount_calculator.py
+++ discount_calculator.py
@@ -2,4 +2,12 @@
 
 
 def compute_discount(bundles: list, products: list, cart_items: str) -> float:
+    """
+    Calculate the total applicable discount to a set of product
+    :param bundles: list
+    :param products: list
+    :param cart_items: str
+    :return: float -> the total discount in euro
+    """
+
     return 0
